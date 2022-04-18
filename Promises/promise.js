ddcf6919001cdfe25542cc1c607993f89db6954d
NatAlpino <nathyalpino@hotmail.com>
2022-04-18 14:47:59 -0300

    Promises

---
+++ Promises/promise.js
@@ -0,0 +1,41 @@
+// Promise síncrona:
+
+const sumNumbers = () => {
+    let result = 1 + 1 
+
+    if (result === 2) {
+        sucessCalback()
+    } else {
+        errorCalback()
+    }
+}
+
+sucessCalback = () => {
+    console.log("Yeah! Number 2!");
+}
+
+errorCalback = () => {
+    console.log("Ops! Somenthing wrong.");
+}
+
+sumNumbers();
+
+// promise assíncrona:
+
+let result = new Promise((resolve, reject) => {
+    let a = 1 + 1 
+    if (a === 2) {
+        resolve("Sucess")
+    } else {
+        reject("Failed")
+    }
+})
+
+result.then((message) => {
+    console.log("This is in the then " + message)
+}).catch((err) => {
+    console.log("This is in the catch " + err)
+})
+
+
+ 
